58ffae09531186151e1c51eaf021d62eee50353b
Damian Krzemiński <damian.krzeminski13@gmail.com>
2023-04-24 16:51:07 +0200

    axios działa  w Profile

--- src/components/Home/HalfContent.js
+++ src/components/Home/HalfContent.js
@@ -1,21 +1,11 @@
-import React, { useEffect, useState} from 'react';
+import React from 'react';
 import './HalfContent.css';
-import axios from "axios";
 
 function HalfContent(){
-    const [data, setData] = useState({data:[]});
-    console.log(data);
-    useEffect(()=> {
-        axios.get("https://api.github.com/users/phantomjs").then((response) => {
-            setData({data: response.data});
-        })
-    }, [])
-
-    return(
-         <div className="HalfContentHome">
-             {data.data.map((item, index) => <span key={index}>{item.login}</span>)}
-         </div>
-    );
+      return (
+        <div className="HalfContentHome">
+        </div>
+      );
 }
 
 export default HalfContent;
--- src/components/Profile/LeftContent.js
+++ src/components/Profile/LeftContent.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import './LeftContent.css';
+import axios from "axios";
 
 function LeftContent(){
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://json.wzium.ovh/mydata.json')
+          .then(response => setData(response.data))
+          .catch(error => console.error(error))
+      }, []);
+
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+
     return(
         <div className="LeftContentProfile">
             <section className='personalDataBox'>
                 <span className='personalDataTextLabel'>Firstname:</span>
-                <span className='personalDataText'>Damian</span>
+                <span className='personalDataText'>{data.firstName}</span>
             </section>
             <section className='personalDataBox'>
                 <span className='personalDataTextLabel'>Lastanme:</span>
-                <span className='personalDataText'>Krzemiński</span>
+                <span className='personalDataText'>{data.lastName}</span>
             </section>
             <section className='personalDataBox'>
                 <span className='personalDataTextLabel'>Group:</span>
-                <span className='personalDataText'>INMN4_PA1.1</span>
+                <span className='personalDataText'>{data.group}</span>
             </section>
             <section className='personalDataBox'>
                 <span className='personalDataTextLabel'>Index Number:</span>
-                <span className='personalDataText'>43737</span>
+                <span className='personalDataText'>{data.indexNumber}</span>
             </section>
         </div>
     );
